d7d01ed3531af55808b0b930ac1c1a2058025beb
juan <chavezvasquezjuan@gmail.com>
2026-07-06 17:21:39 -0500

    fix: corregir ruta base para github pages

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: '/portfolio/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
